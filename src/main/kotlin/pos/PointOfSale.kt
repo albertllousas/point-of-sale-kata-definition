7649cadf4a65ed2f3b1620272fdb6cf1f7e841c6
Egor Bujinov <egor.bujinov@external-n26.com>
2022-12-15 14:28:26 +0100

    Fix 1st test

--- src/main/kotlin/pos/PointOfSale.kt
+++ src/main/kotlin/pos/PointOfSale.kt
@@ -2,5 +2,12 @@ package pos
 
 class PointOfSale {
 
-    fun scan(productId: String): String = TODO()
+    fun scan(productId: String): String =
+        products.getOrDefault(productId, "invalid product")
+
+    companion object {
+        val products = mapOf(
+                "901234" to "1.50"
+        )
+    }
 }

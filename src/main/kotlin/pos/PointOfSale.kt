113596e9b2f7cd53fbcaba2ef2d19121860b4bb3
vladimir-shevchenko-n26 <vladimir.shevcheko@n26.com>
2022-12-15 15:38:05 +0100

    commit

--- src/main/kotlin/pos/PointOfSale.kt
+++ src/main/kotlin/pos/PointOfSale.kt
@@ -2,11 +2,15 @@ package pos
 
 class PointOfSale {
 
-    private val productMap = mapOf(ProductId(123456) to Display("1.50"))
+    private val productMap = mapOf(ProductId("123456") to Display("1.50"))
 
-    fun scan(productId: ProductId): Display? = productMap[productId]
+    fun scan(productId: ProductId): Display = productMap[productId] ?: Display.invalidProductDisplay
 
-    data class ProductId(val id: Number)
+    data class ProductId(val id: String)
 
-    data class Display(val price: String)
+    data class Display(val text: String) {
+        companion object {
+            val invalidProductDisplay: Display = Display("invalid product")
+        }
+    }
 }
--- src/test/kotlin/pos/PointOfSaleTest.kt
+++ src/test/kotlin/pos/PointOfSaleTest.kt
@@ -8,16 +8,27 @@ class PointOfSaleTest {
     @Test
     fun `when a product is scanned, the price is displayed`() {
 
-        val result = PointOfSale().scan(PointOfSale.ProductId(123456))
+        val result = PointOfSale().scan(PointOfSale.ProductId("123456"))
 
         assertThat(result).isEqualTo(PointOfSale.Display("1.50"))
     }
 
     @Test
     fun `when a scanned product is not present, a message 'invalid product' is displayed`() {
-        val result = PointOfSale().scan(PointOfSale.ProductId(654321))
+        val result = PointOfSale().scan(PointOfSale.ProductId("654321"))
 
         assertThat(result).isEqualTo(PointOfSale.Display("invalid product"))
+    }
+
+    @Test
+    fun `when several products are scanned, a shopping cart should contain them all`() {
+        val pointOfSale = PointOfSale()
+
+        pointOfSale.scan(PointOfSale.ProductId("123456"))
+        pointOfSale.scan(PointOfSale.ProductId("654321"))
+        pointOfSale.scan(PointOfSale.ProductId("123456"))
+
+        assertThat(pointOfSale.listProducts().size).isEqualTo(3)
     }
 
 }

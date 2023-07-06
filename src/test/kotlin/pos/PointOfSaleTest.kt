d4dfd8146c96ee5bdf8de7122c69d63e5ad47670
sheetalumathe11 <sheetal.umathe@n26.com>
2023-07-06 15:18:30 +0200

    [TDD-001] refactor pos invalid product test

--- src/test/kotlin/pos/PointOfSaleTest.kt
+++ src/test/kotlin/pos/PointOfSaleTest.kt
@@ -6,18 +6,26 @@ import org.junit.jupiter.api.Test
 
 class PointOfSaleTest {
 
+    private val pointOfSale = PointOfSale(products = listOf(
+        PointOfSale.Product(
+            productId = "123456",
+            price = BigDecimal("12.50"),
+            description = "laptop"
+        )
+    ))
+
     @Test
     fun `should display product price when scanned`() {
-        val pointOfSale = PointOfSale(products = listOf(
-            PointOfSale.Product(
-                productId = "123456",
-                price = BigDecimal("12.50"),
-                description = "laptop"
-            )
-        ))
-
         val result = pointOfSale.scan("123456")
 
         assertThat(result).isEqualTo("12.50")
     }
+
+    @Test
+    fun `should display invalid product message when scanned product is not found`() {
+        val result = pointOfSale.scan("1234")
+
+        assertThat(result).isEqualTo("invalid product")
+    }
+
 }

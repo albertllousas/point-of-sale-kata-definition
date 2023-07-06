e1926f91c8b37412c6c88e2d50aa8bf216263bda
sheetalumathe11 <sheetal.umathe@n26.com>
2023-07-06 17:15:36 +0200

    [TDD-001] pos

--- src/test/kotlin/pos/PointOfSale.kt
+++ src/test/kotlin/pos/PointOfSale.kt
@@ -5,26 +5,27 @@ import java.math.BigDecimal
 
 class PointOfSale(val products: List<Product>) {
 
-  private val currentShoppingCart: MutableList<Product> = mutableListOf()
-  fun scan(productId: String): Display =
-      products.findById(productId)
-          ?.also { currentShoppingCart.add(it) }
-          ?.price?.let(::Price) ?: Error("invalid product")
+    private val currentShoppingCart: MutableList<Product> = mutableListOf()
+    fun scan(productId: String): Display =
+        products.findById(productId)
+            ?.also { currentShoppingCart.add(it) }
+            ?.price?.let(::Price) ?: Error("invalid product")
 
-  fun currentShoppingCart(): List<String> = currentShoppingCart.map { it.productId }
+    fun currentShoppingCart(): List<String> = currentShoppingCart.map { it.productId }
 
-  fun remove(productId: String): Display =
-      products.findById(productId)
-          ?.also { currentShoppingCart.remove(it) }
-          ?.price?.negate()?.let(::Price) ?: Error("invalid product")
+    fun remove(productId: String): Display =
+        products.findById(productId)
+            ?.also { currentShoppingCart.remove(it) }
+            ?.price?.negate()?.let(::Price) ?: Error("invalid product")
 
-  data class Product(val productId: String, val price: BigDecimal, val description: String)
+    data class Product(val productId: String, val price: BigDecimal, val description: String)
 
-  private fun List<Product>.findById(productId: String) = this.find { it.productId == productId }
-  fun cancel() = currentShoppingCart.clear()
-}
+    private fun List<Product>.findById(productId: String) = this.find { it.productId == productId }
+    fun cancel() = currentShoppingCart.clear()
+
+    fun checkOut(): Price = currentShoppingCart.map { it.price }.sumOf { it }.let(::Price)}
 
 sealed class Display {
-  data class Price(val value: BigDecimal) : Display()
-  data class Error(val value: String) : Display()
+    data class Price(val value: BigDecimal) : Display()
+    data class Error(val value: String) : Display()
 }
--- src/test/kotlin/pos/PointOfSaleTest.kt
+++ src/test/kotlin/pos/PointOfSaleTest.kt
@@ -1,72 +1,84 @@
 package pos
 
+import java.math.BigDecimal
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import pos.PointOfSale.Product
-import java.math.BigDecimal
 
 class PointOfSaleTest {
 
-  private val pointOfSale = PointOfSale(products = listOf(
-      Product(productId = "123456", price = BigDecimal("12.50"), description = "laptop"),
-      Product(productId = "121212", price = BigDecimal("99.99"), description = "iPhone"),
-  ))
+    private val pointOfSale = PointOfSale(
+        products = listOf(
+            Product(productId = "123456", price = BigDecimal("12.50"), description = "laptop"),
+            Product(productId = "121212", price = BigDecimal("99.99"), description = "iPhone"),
+        )
+    )
 
-  @Test
-  fun `should display product price when scanned`() {
-    val result = pointOfSale.scan("123456")
+    @Test
+    fun `should display product price when scanned`() {
+        val result = pointOfSale.scan("123456")
 
-    assertThat(result).isEqualTo(Display.Price(BigDecimal("12.50")))
-  }
+        assertThat(result).isEqualTo(Display.Price(BigDecimal("12.50")))
+    }
 
-  @Test
-  fun `should display invalid product message when scanned product is not found`() {
-    val result = pointOfSale.scan("1234")
+    @Test
+    fun `should display invalid product message when scanned product is not found`() {
+        val result = pointOfSale.scan("1234")
 
-    assertThat(result).isEqualTo(Display.Error("invalid product"))
-  }
+        assertThat(result).isEqualTo(Display.Error("invalid product"))
+    }
 
-  @Test
-  fun `should add a product to the current digital shopping cart when scanned successfully`() {
-    pointOfSale.scan("123456")
-    pointOfSale.scan("121212")
+    @Test
+    fun `should add a product to the current digital shopping cart when scanned successfully`() {
+        pointOfSale.scan("123456")
+        pointOfSale.scan("121212")
 
-    val result = pointOfSale.currentShoppingCart()
+        val result = pointOfSale.currentShoppingCart()
 
-    assertThat(result).isEqualTo(listOf("123456", "121212"))
-  }
+        assertThat(result).isEqualTo(listOf("123456", "121212"))
+    }
 
-  @Test
-  fun `should remove product from shopping cart and display price in negative when added mistakenly`() {
-    pointOfSale.scan("123456")
-    pointOfSale.scan("121212")
+    @Test
+    fun `should remove product from shopping cart and display price in negative when added mistakenly`() {
+        pointOfSale.scan("123456")
+        pointOfSale.scan("121212")
 
-    val result = pointOfSale.remove("123456")
+        val result = pointOfSale.remove("123456")
 
-    assertThat(result).isEqualTo(Display.Price(BigDecimal("-12.50")))
-    assertThat(pointOfSale.currentShoppingCart()).isEqualTo(listOf("121212"))
-  }
+        assertThat(result).isEqualTo(Display.Price(BigDecimal("-12.50")))
+        assertThat(pointOfSale.currentShoppingCart()).isEqualTo(listOf("121212"))
+    }
 
-  @Test
-  fun `should only remove one product if shopping cart contains the same product multiple times`() {
-    pointOfSale.scan("123456")
-    pointOfSale.scan("121212")
-    pointOfSale.scan("121212")
-    pointOfSale.remove("121212")
+    @Test
+    fun `should only remove one product if shopping cart contains the same product multiple times`() {
+        pointOfSale.scan("123456")
+        pointOfSale.scan("121212")
+        pointOfSale.scan("121212")
+        pointOfSale.remove("121212")
 
-    val result = pointOfSale.currentShoppingCart()
+        val result = pointOfSale.currentShoppingCart()
 
-    assertThat(result).isEqualTo(listOf("123456", "121212"))
-  }
+        assertThat(result).isEqualTo(listOf("123456", "121212"))
+    }
 
-  @Test
-  fun `should cancel the current sale in any moment, clearing out the cart`() {
-    pointOfSale.scan("123456")
-    pointOfSale.scan("121212")
-    pointOfSale.scan("121212")
+    @Test
+    fun `should cancel the current sale in any moment, clearing out the cart`() {
+        pointOfSale.scan("123456")
+        pointOfSale.scan("121212")
+        pointOfSale.scan("121212")
 
-    pointOfSale.cancel()
+        pointOfSale.cancel()
 
-    assertThat(pointOfSale.currentShoppingCart()).isEqualTo(emptyList<String>())
-  }
+        assertThat(pointOfSale.currentShoppingCart()).isEqualTo(emptyList<String>())
+    }
+
+    @Test
+    fun `should check out and display the total price`() {
+      pointOfSale.scan("123456")
+      pointOfSale.scan("121212")
+
+      val result = pointOfSale.checkOut()
+
+      assertThat(result).isEqualTo(Display.Price(BigDecimal("112.49")))
+    }
 }

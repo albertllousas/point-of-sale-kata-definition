dce023ed836210ce49547c496a0dc2e3785d3812
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2023-07-06 16:52:10 +0200

    Almost done

--- src/test/kotlin/pos/PointOfSale.kt
+++ src/test/kotlin/pos/PointOfSale.kt
@@ -1,22 +1,30 @@
 package pos
 
+import pos.Display.*
 import java.math.BigDecimal
 
 class PointOfSale(val products: List<Product>) {
 
-    private val currentShoppingCart: MutableList<Product> = mutableListOf()
-    fun scan(productId: String): String =
-        products.findById(productId)
-            ?.also { currentShoppingCart.add(it) }
-            ?.price?.toString() ?: "invalid product"
+  private val currentShoppingCart: MutableList<Product> = mutableListOf()
+  fun scan(productId: String): Display =
+      products.findById(productId)
+          ?.also { currentShoppingCart.add(it) }
+          ?.price?.let(::Price) ?: Error("invalid product")
 
-    fun currentShoppingCart(): List<String> = currentShoppingCart.map { it.productId }
+  fun currentShoppingCart(): List<String> = currentShoppingCart.map { it.productId }
 
-    fun remove(productId: String): String =
-          products.findById(productId)
-                ?.also { currentShoppingCart.remove(it) }
-                ?.price?.negate()?.toString() ?: "invalid product"
+  fun remove(productId: String): Display =
+      products.findById(productId)
+          ?.also { currentShoppingCart.remove(it) }
+          ?.price?.negate()?.let(::Price) ?: Error("invalid product")
 
-    data class Product(val productId: String, val price: BigDecimal, val description: String)
-    private fun List<Product>.findById(productId: String) = this.find { it.productId == productId }
+  data class Product(val productId: String, val price: BigDecimal, val description: String)
+
+  private fun List<Product>.findById(productId: String) = this.find { it.productId == productId }
+  fun cancel() = currentShoppingCart.clear()
+}
+
+sealed class Display {
+  data class Price(val value: BigDecimal) : Display()
+  data class Error(val value: String) : Display()
 }
